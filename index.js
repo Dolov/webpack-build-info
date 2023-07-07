258152478d65ee9017c89c03724fa90681b7cef8
石松岩.前端架构组 <songyan_shi@163.com>
2023-07-07 11:15:18 +0800

    feat: 支持自定义参数 触发方式

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const child_process = require('child_process');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const PLUGIN_NAME = 'WebpackBuildInfo'
+const DEFAULT_SCRIPT_ID = 'webpack-build-info'
+
+const NAME_STYLE = "background:#35495e; padding: 2px 5px;  color: #fff"
+const VALUE_STYLE = "background:#41b883; padding: 2px 5px;  color: #fff"
+
 /** 获取 git 版本信息 */
 const getGitInfo = () => {
 	try {
 		const version = child_process.execSync('git branch --v', {
 			encoding: 'utf8'
 		});
-		return version.replace('\n', '').slice(1)
+		return version.replace('\n', '').slice(2)
 	} catch (error) {
 		return null
 	}
 }
+
+/** 输出带有样式的 log */
+const styleLog = (...args) => {
+	const { values, styles } = args.reduce((previousValue, currentValue) => {
+		let item = currentValue
+		if (typeof currentValue === 'string') {
+			item = {
+				value: currentValue,
+				style: ""
+			}
+		}
+		const { value, style } = item
+		previousValue.values.push(value)
+		previousValue.styles.push(`"${style}"`)
+		return previousValue
+	}, { values: [], styles: [] })
+	const valueStr = values.map(item => `%c${item}`).join('')
+	const styleStr = styles.join()
+	return `console.log('${valueStr}', ${styleStr});\n`
+}
+
 
 class WebpackBuildInfo {
 
@@ -21,32 +48,68 @@ class WebpackBuildInfo {
 
 	apply(compiler) {
 
-		const { format, scriptId = "webpack-build-info" } = this.options
 		const gitInfo = getGitInfo()
+		const { format, scriptId = DEFAULT_SCRIPT_ID, trigger, extraParams } = this.options
 
 		/** 构建开始时间 */
 		const startTime = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
 
-		compiler.hooks.compilation.tap('WebpackBuildInfo', compilation => {
+		compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
 
-			HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('WebpackBuildInfo', (data, cb) => {
+			HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(PLUGIN_NAME, (data, cb) => {
 				let script = ''
 				if (gitInfo) {
-					script += `console.log("%c版本信息", "color: #1677ff; text-decoration: underline;font-weight: 900;", '${gitInfo}');`
-				}
-				script += `console.log("%c构建开始时间", "color: #52c41a; text-decoration: underline;font-weight: 900;", '${startTime}');`
 
-				/** 构建结束时间 */
+					/** 输出版本信息 */
+					script += styleLog(
+						{ value: "版本信息", style: NAME_STYLE },
+						{ value: gitInfo, style: VALUE_STYLE },
+					)
+				}
+
+				/** 输出构建开始时间 */
+				script += styleLog(
+					{ value: "构建开始时间", style: NAME_STYLE },
+					{ value: startTime, style: VALUE_STYLE },
+				)
+
+				/** 输出构建结束时间 */
 				const endTime = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
-				script += `console.log("%c构建结束时间", "color: #13c2c2; text-decoration: underline;font-weight: 900;", '${endTime}');`
+				script += styleLog(
+					{ value: "构建结束时间", style: NAME_STYLE },
+					{ value: endTime, style: VALUE_STYLE },
+				)
+
+				/** 自定义参数 */
+				if (Array.isArray(extraParams)) {
+					extraParams.forEach(item => {
+						const { name, value, nameStyle = NAME_STYLE, valueStyle = VALUE_STYLE } = item
+						script += styleLog(
+							{ value: name, style: nameStyle },
+							{ value: value, style: valueStyle },
+						)
+					})
+				}
 
 				/** 自定义日志格式 */
 				if (format) {
 					script = formatLog({
 						gitInfo,
-						startTime,
 						endTime,
+						startTime,
+						extraParams,
 					})
+				}
+
+				/** 在控制台输入指定变量后再输出构建信息 */
+				if (trigger) {
+					script = `
+						Object.defineProperty(window, '${trigger}', {
+							get() {
+								${script}
+							}
+						})
+					`
 				}
 
 				// 修改 HTML 内容
